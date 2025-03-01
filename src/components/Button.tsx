fe4b6131b72653fd8cc05fa3e26d11b401fb6367
Ali zain javed <alizainjaved@yahoo.com>
2025-03-02 01:29:22 +0500

    feature:create common button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type ButtonProps = {
+  children: React.ReactNode;
+  onClick: () => void;
+  className?: string;
+};
+
+const Button: React.FC<ButtonProps> = ({ children, onClick, className = '' }) => {
+  return (
+    <button className={`px-4 py-2 bg-blue-500 text-white rounded ${className}`} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
