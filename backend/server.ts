75166e0cca87b1f1d812e18c81e6d2161aa22d85
Ali zain javed <alizainjaved29@gmail.com>
2025-03-02 03:18:46 +0500

    Added get request for fetching care notes list

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,24 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
+import fs from 'fs';
+import path from 'path';
 
 const app = express();
 const PORT = 3001;
+const DATA_FILE = path.join(__dirname, 'data.json');
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/care-notes', (req: Request, res: Response) => {
+  fs.readFile(DATA_FILE, 'utf-8', (err, data) => {
+    console.log('data', data);
+    if (err) {
+      return res.status(500).json({ error: 'Failed to read data file' });
+    }
+    res.json(JSON.parse(data));
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
